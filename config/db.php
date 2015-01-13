e7c5c5574508cf6a20d49733b37212459e4d4e66
artemzakholodilo <azaholodilo@gmail.com>
2015-01-13 15:33:43 +0200

    first commit

---
+++ config/db.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=kttsoft',
+    'username' => 'root',
+    'password' => '',
+    'charset' => 'utf8',
+];
